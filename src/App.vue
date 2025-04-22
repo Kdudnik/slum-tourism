daf446df714f5b2fc5053c7b410e725efad665e6
Kiril Dudnik <kdudnik17@gmail.com>
2025-04-22 23:39:34 +0200

    wip: header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <script setup>
-import { useI18n } from "vue-i18n";
-
-const { locale, availableLocales } = useI18n();
-
-function switchLanguage() {
-  const localeIndex = availableLocales.findIndex((el) => el === locale.value);
-  if (localeIndex + 1 >= availableLocales.length)
-    locale.value = availableLocales[0];
-  else locale.value = availableLocales[localeIndex + 1];
-}
+import HeaderComponent from "./components/HeaderComponent.vue";
 </script>
 
 <template>
-  <h1>{{ $t("test") }}</h1>
-  <button
-  class="border border-black cursor-pointer rounded-sm"
-  @click="switchLanguage">Change lang</button>
+  <HeaderComponent />
 </template>
-
-<style scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { locale, availableLocales } = useI18n();
+const listItems = ref([
+  {message: "Home"},
+  {message: "Background Info"},
+  {message: "Our Tour"},
+  {message: "Reviews"},
+])
+
+function switchLanguage() {
+  const localeIndex = availableLocales.findIndex((el) => el === locale.value);
+  if (localeIndex + 1 >= availableLocales.length)
+    locale.value = availableLocales[0];
+  else locale.value = availableLocales[localeIndex + 1];
+}
+</script>
+
+<template>
+  <header>
+    <container class="my-10">
+      <nav class="flex justify-between items-center">
+        <div>Site Name</div>
+
+        <div class="flex">
+          <ul class="flex">
+            <li v-for="item in listItems">
+              {{ item.message }}
+            </li>
+          </ul>
+          <button type="button" class="btn">{{ $i18n.locale }}</button>
+        </div>
+      </nav>
+    </container>
+  </header>
+</template>
