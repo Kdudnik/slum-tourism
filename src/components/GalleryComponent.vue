921c32205c0a98200198474c1c494e1b727e0651
Kiril Dudnik <kdudnik17@gmail.com>
2025-04-23 10:36:44 +0200

    wip: gallery-section

---
+++ src/components/GalleryComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+import GalleryBlockComponent from './GalleryBlockComponent.vue';
+
+const items = ref([
+  {
+    text: "Photo: Reality Tours and Travel",
+    img: "/gallery/gallery-1.png"
+  },
+  {
+    text: "Photo: Tripadvisor",
+    img: "/gallery/gallery-2.png"
+  },
+  {
+    text: "Photo: www.shutterstock.com",
+    img: "/gallery/gallery-3.png"
+  },
+])
+</script>
+
+<template>
+  <section class="mt-25">
+    <container>
+      <div>
+        <GalleryBlockComponent v-for="item in items" />
+      </div>
+    </container>
+  </section>
+</template>
